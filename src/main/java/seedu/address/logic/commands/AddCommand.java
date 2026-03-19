e32e413146491737c27fef91ea5f3185c5b9533a
Sinnez1 <99955247+Sinnez1@users.noreply.github.com>
2026-03-19 14:38:27 +0800

    Edit wrong matric number in sample message

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -31,7 +31,7 @@ public class AddCommand extends Command {
             + PREFIX_NAME + "John Doe "
             + PREFIX_PHONE + "98765432 "
             + PREFIX_EMAIL + "johnd@example.com "
-            + PREFIX_MATRIC_NUMBER + "A1234567Z"
+            + PREFIX_MATRIC_NUMBER + "A1234567X "
             + PREFIX_TAG + "friends "
             + PREFIX_TAG + "owesMoney";
 
